8c226930e5570000dbd80ff883f0e144c543ddee
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-01-23 01:37:50 -0300

    feajustes relação consulta

--- DTO/Dominio/RelacaoConsulta.cs
+++ DTO/Dominio/RelacaoConsulta.cs
@@ -7,6 +7,14 @@ namespace BibliotecaViva.DTO.Dominio
 {
     public class RelacaoConsulta : IDisposable
     {
+        public RelacaoConsulta()
+        {
+            
+        }
+        public RelacaoConsulta(int codigoRegistro)
+        {
+            CodRegistro = codigoRegistro;
+        }
         public int CodRegistro { get; set; }
 
         public void Dispose()
